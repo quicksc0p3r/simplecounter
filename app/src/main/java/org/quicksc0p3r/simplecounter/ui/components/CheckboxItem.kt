8ebd61f66fc5731998ba2364d0dad451c29d7b3c
quicksc0p3r <80166934+quicksc0p3r@users.noreply.github.com>
2023-12-02 11:52:12 +0500

    Create CheckboxItem.kt

---
+++ app/src/main/java/org/quicksc0p3r/simplecounter/ui/components/CheckboxItem.kt
@@ -0,0 +1,36 @@
+package org.quicksc0p3r.simplecounter.ui.components
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CheckboxItem(
+    name: String,
+    isToggled: Boolean,
+    changeValue: (Boolean) -> Unit
+) {
+    ListItem(
+        modifier = Modifier
+            .clip(RoundedCornerShape(10.dp))
+            .toggleable(
+                value = isToggled,
+                onValueChange = changeValue,
+                role = Role.Checkbox
+            )
+            .height(60.dp),
+        leadingContent = {
+            Checkbox(
+                checked = isToggled,
+                onCheckedChange = null
+            )
+        },
+        headlineContent = { Text(name) }
+    )
+}
